091255fadba58f83f70a00f3056f91bde01cc3cc
Tomas <achmedovas.tomas@gmail.com>
2016-03-21 14:22:18 +0200

    Update coingate.php

--- catalog/language/english/payment/coingate.php
+++ catalog/language/english/payment/coingate.php
@@ -2,4 +2,4 @@
 
 $_['button_confirm'] = 'Confirm order';
 $_['heading_title'] = 'Bitcoin via CoinGate.com';
-$_['text_title'] = 'With Bitcoin via CoinGate.com';
+$_['text_title'] = 'Bitcoin via CoinGate';
