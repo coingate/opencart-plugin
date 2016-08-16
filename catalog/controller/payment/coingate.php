263b48fddb81c47ed98fa899c115e6a148db56a4
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2016-08-16 17:33:11 +0300

    Add version to UA

--- catalog/controller/payment/coingate.php
+++ catalog/controller/payment/coingate.php
@@ -18,7 +18,7 @@ class ControllerPaymentCoingate extends Controller
       'api_key'     => $this->config->get('coingate_api_key'),
       'api_secret'  => $this->config->get('coingate_api_secret'),
       'environment' => $this->config->get('coingate_test') == 1 ? 'sandbox' : 'live',
-      'user_agent'  => 'CoinGate - OpenCart Extension v' . COINGATE_OPENCART_EXTENSION_VERSION
+      'user_agent'  => 'CoinGate - OpenCart v'.VERSION.' Extension v' . COINGATE_OPENCART_EXTENSION_VERSION
     ));
 
     $this->load->language('payment/coingate');
