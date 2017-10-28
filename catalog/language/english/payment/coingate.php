bfc0c2f7be8555a13362a3dff8d1ba0d68e8e3cc
ibaciulis <i.baciulis@gmail.com>
2017-10-28 16:33:28 +0300

    Update heading_title and text_title

--- catalog/language/english/payment/coingate.php
+++ catalog/language/english/payment/coingate.php
@@ -1,5 +1,5 @@
 <?php
 
 $_['button_confirm'] = 'Confirm order';
-$_['heading_title'] = 'Bitcoin via CoinGate.com';
-$_['text_title'] = 'Bitcoin via CoinGate';
+$_['heading_title'] = 'Bitcoin and Altcoins via CoinGate';
+$_['text_title'] = 'Pay with Bitcoin or Altcoins via CoinGate.com';
