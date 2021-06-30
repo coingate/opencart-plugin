55f42527fa5ec3e3e48f895781982bb4fc9cd2e6
stranac <stranac@hotmail.com>
2021-06-30 12:36:20 +0200

    Modify code table SQL

--- upload/admin/model/extension/payment/coingate.php
+++ upload/admin/model/extension/payment/coingate.php
@@ -15,7 +15,7 @@ class ModelExtensionPaymentCoingate extends Model {
     // Modify the currency table to allow 4-character currency code
     $this->db->query("
       ALTER TABLE `" . DB_PREFIX . "currency`
-      MODIFY `code` VARCHAR(3) NOT NULL;
+      MODIFY `code` VARCHAR(4) COLLATE utf8_general_ci NOT NULL;
     ");
 
     $this->load->model('setting/setting');
